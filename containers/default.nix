699f96779b4ddd289e73af52b276046b17087796
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2025-11-29 16:02:15 -0600

    Enhance documentation for configuration imports in default.nix

--- containers/default.nix
+++ containers/default.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  /*
+    Configuration Imports
+    Import configurations for specific services.
+
+    Each imported file contains the Caddy configuration for a specific service, such as a homepage or a Minecraft server.
+  */
   imports = [
     # ./homepage.nix
     # ./minecraft.nix
